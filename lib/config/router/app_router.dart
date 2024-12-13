b20d75e851bf536831e47fa2f520dfeb9dc7bf61
EriOsoy <eosoy@CentroHistorico.local>
2024-12-13 11:38:14 -0600

    first commit

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,28 @@
+import 'package:go_router/go_router.dart';
+
+import '../../features/auth/presentation/screens/login_screen.dart';
+import '../../features/auth/presentation/screens/register_screen.dart';
+import '../../features/products/presentation/screens/products_screen.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    ///* Auth Routes
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => const LoginScreen(),
+    ),
+    GoRoute(
+      path: '/register',
+      builder: (context, state) => const RegisterScreen(),
+    ),
+
+    ///* Product Routes
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const ProductsScreen(),
+    ),
+  ],
+
+  ///! TODO: Bloquear si no se está autenticado de alguna manera
+);
